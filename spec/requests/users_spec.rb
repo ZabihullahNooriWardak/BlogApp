63797b8412cdc6bcdc8c700d378a63b27f7709b8
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-12-07 17:45:52 +0430

    removed extra puts statements

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,20 +3,17 @@ require 'rails_helper'
 describe 'GET /index' do
   it 'returns a successful response' do
     get users_url
-    puts response.status
     expect(response).to be_successful
   end
 
   it 'renders a correct template' do
     get users_url
-    puts response.status
     template = File.read('app/views/users/index.html.erb')
     expect(response.body).to match(/#{template}/)
   end
 
   it 'includes the correct placeholder' do
     get users_url
-    puts response.status
     placeholder = 'All users page'
     expect(response.body).to match(/#{placeholder}/)
   end
@@ -25,20 +22,17 @@ end
 describe 'GET /show' do
   it 'returns a successful response' do
     get '/users/1'
-    puts response.status
     expect(response).to be_successful
   end
 
   it 'renders a correct template' do
     get '/users/1'
-    puts response.status
     template = File.read('app/views/users/show.html.erb')
     expect(response.body).to match(/#{template}/)
   end
 
   it 'includes the correct placeholder' do
     get '/users/1'
-    puts response.status
     placeholder = 'Single user page'
     expect(response.body).to match(/#{placeholder}/)
   end
